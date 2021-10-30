d1d99341056c153c9043423b1aa54f8213b37305
Roshni Biswas <rbiswas@ds.dfci.harvard.edu>
2021-10-30 14:15:32 -0400

    remove unused imports

--- cassandra/prediction.py
+++ cassandra/prediction.py
@@ -1,9 +1,7 @@
 import numpy as np
 import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import (accuracy_score, classification_report,
-                             confusion_matrix, precision_score, recall_score)
+from sklearn.metrics import accuracy_score, precision_score, recall_score
 from sklearn.model_selection import train_test_split
 
 
